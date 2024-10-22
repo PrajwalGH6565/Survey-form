bff86b23116e9837efabbd984cb35025bced556d
Prajwal <prajwalgh23@gmail.com>
2024-10-22 21:27:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const surveyForm = document.getElementById("surveyForm");
+const result = document.getElementById("result");
+const responseName = document.getElementById("responseName");
+const responseEmail = document.getElementById("responseEmail");
+const responseFeedback = document.getElementById("responseFeedback");
+const responseRatings = document.getElementById("responseRatings");
+
+
+surveyForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const feedback = document.getElementById("feedback").value;
+  const rating = document.getElementById("ratings").value;
+
+  responseName.textContent = `Name: ${name}`;
+  responseEmail.textContent = `Email: ${email}`;
+  responseFeedback.textContent = `Feedback: ${feedback}`;
+  responseRatings.textContent = `Ratings: ${rating}`;
+
+  result.classList.remove("hidden");
+});
